95693631d1d49bbfe629c79b92d3e9eb33a4ab54
evertonmoraes <evertondasilvamoraes@gmail.com>
2024-05-31 14:38:05 -0300

    Versão 1.0

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
 	* <h1>Conta Terminal</h1>
 	*Simulação de Conta Terminal onde recebe variáveis atráves do terminal utilizando a classe scanner e posteriormente exibindo os dados capturados
-	* <p>
+	* 
 	* <b>Note:</b> Projeto Módulo "Dominango a Linguagem de Programaação Java" do BootCamp Santander 2024 - Backend com Java"
 	*
 	* @author  Ëverton Moraes
@@ -16,7 +16,7 @@ public class ContaTerminal {
 	   * Este método é para capturar dados refentes a uma conta bancária e posteriormente exibir uma mensagem confirmando a 'criação' da conta com os dados capturados
 	   * @param numeroConta Número da Conta que foi criada (int)
 	   * @param numeroAgencia Número da Agência quem que a Conta foi criada (String)
-       * @param nomeCliente Primeiro Nome do Titular da Conta criada (String)
+       * @param nomeCliente Nome do Titular da Conta criada (String)
        * @param saldoInicial Saldo Inicial da Conta criada (double)
 	   * @return int o resultado deste método é a soma dos dois números.
 	   */
@@ -26,17 +26,20 @@ public class ContaTerminal {
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
     //recebendo as variaveis
-    System.out.println("Por favor, digite o número da Conta");
+    System.out.println("Por favor, digite o número da Conta:");
     int numeroConta = scanner.nextInt();
 
-    System.out.println("Por favor, digite o número da Agência");
+    System.out.println("Por favor, digite o número da Agência:");
     String numeroAgencia = scanner.next();
+    
+    //tratativa para pegar o nome com espaço
+    scanner.nextLine();
 
-    System.out.println("Por favor, digite o Primeiro Nome do Cliente");
-    String nomeCliente = scanner.next();
+    System.out.println("Por favor, digite o Nome do Cliente:");
+    String nomeCliente = scanner.nextLine();
 
-    System.out.println("Por favor, digite o Saldo Inicial");
-    double saldoInicial = scanner.nextDouble();
+    System.out.println("Por favor, digite o Saldo Inicial:");
+    double saldoInicial = scanner.nextDouble(); 
 
     //imprimindo a consulta final
     System.out.println("Olá "+nomeCliente+", \nobrigado por criar uma conta em nosso banco, sua agência é '"+numeroAgencia+"', conta '"+numeroConta+"' e seu saldo 'R$ "+saldoInicial+"'' já está disponível para saque");
